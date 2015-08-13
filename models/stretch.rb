72706fb03f85bd4b31c43f500b3197c5122e467a
Mimi Bancod-Buan <mimibancodbuan@gmail.com>
2015-08-13 18:45:54 +0000

    added content

--- models/stretch.rb
+++ models/stretch.rb
@@ -1,9 +1,11 @@
 def stretch(area)
   if area == "legs"
-    return ["legs1", "legs2", "legs3"]
+    return ["<iframe width='560' height='315' src='https://www.youtube.com/embed/9ZFJpaOD3Cc' frameborder='0' allowfullscreen>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/9CMu5hyTcg0' frameborder='0' allowfullscreen>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/qZTGgEWPbLk' frameborder='0' allowfullscreen>"]
   elsif area == "arms"
     return ["arms1", "arms2", "arms3"]
   elsif area == "back"
     return ["back1", "back2", "back3"]
+  elsif area == "abdominal"
+    return ["abs1", "abs2", "abs3"]
   end
 end
