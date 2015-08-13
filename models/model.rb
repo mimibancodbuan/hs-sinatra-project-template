00334e7c2d8576bd46b139af9937b846c97e5a83
sdspj <p_a_k_@hotmail.com>
2015-08-13 17:42:03 +0000

    made changes

---
+++ models/model.rb
@@ -0,0 +1,27 @@
+
+# def stretch(area, difficulty)
+  
+# easyarm = ["easy arm stretch1", "easy arm stretch2", "easy arm stretch3"]
+
+#   if area == "arm"
+#     if difficulty == "easy"
+#       puts easyarm.sample
+#     elsif difficulty == "medium"
+#       puts "medium arm stretch"
+#     elsif difficuly == "hard"
+#       puts "hard arm stretch"
+#     end
+#   end
+  
+#   if area == "leg"
+#     if difficulty == "easy"
+#       puts "easy leg stretch"
+#     elsif difficulty == "medium"
+#       puts "medium leg stretch"
+#     elsif difficuly == "hard"
+#       puts "hard leg stretch"
+#     end
+#   end
+# end
+
+# stretch("arm", "easy")
