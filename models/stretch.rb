ed3d535981a1e5366e71310cdf8be298f5c16b2a
sdspj <p_a_k_@hotmail.com>
2015-08-14 13:46:43 +0000

    aboot m3

--- models/stretch.rb
+++ models/stretch.rb
@@ -2,10 +2,10 @@ def stretch(area)
   if area == "legs"
     return ["<iframe width='560' height='315' src='https://www.youtube.com/embed/9ZFJpaOD3Cc' frameborder='0' allowfullscreen>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/9CMu5hyTcg0' frameborder='0' allowfullscreen>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/qZTGgEWPbLk' frameborder='0' allowfullscreen>"]
   elsif area == "arms"
-    return ["arms1", "arms2", "arms3"]
+    return ["<iframe width='560' height='315' src='https://www.youtube.com/embed/mN5gbzsPJ1Y' frameborder='0' allowfullscreen></iframe>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/ihOqwYV0_Ko' frameborder='0' allowfullscreen></iframe>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/c1pIz1lcnA8' frameborder='0' allowfullscreen></iframe>"]
   elsif area == "back"
-    return ["back1", "back2", "back3"]
+    return ["<iframe width='560' height='315' src='https://www.youtube.com/embed/CO3racIlTcg' frameborder='0' allowfullscreen></iframe>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/FsKHQ81Cbdk' frameborder='0' allowfullscreen></iframe>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/3kSdDF2cRkI' frameborder='0' allowfullscreen></iframe>"]
   elsif area == "abdominal"
-    return ["abs1", "abs2", "abs3"]
+    return ["<iframe width='560' height='315' src='https://www.youtube.com/embed/z9svTmJ1LEY' frameborder='0' allowfullscreen></iframe>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/0HHS8Z6IdI8' frameborder='0' allowfullscreen></iframe>", "<iframe width='560' height='315' src='https://www.youtube.com/embed/v0ELTWpEGSk' frameborder='0' allowfullscreen></iframe>"]
   end
 end
